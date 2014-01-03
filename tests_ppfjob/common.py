c92e9bb08b4175d445ce8b1875f3778892c3e83e
ptmono <ptmono@gmail.com>
2014-01-04 06:10:14 +0900

    Added tests_ppfjob

---
+++ tests_ppfjob/common.py
@@ -0,0 +1,11 @@
+import os, sys
+
+from dlibs.libs import add_sys_path
+ROOT_PATH = add_sys_path(__file__, subpath=True)
+
+sys.path.insert(0, '/home/ptmono/works/0git/dScrapper')
+
+from unittest import TestCase
+
+def data_path(filename):
+    return os.path.join('data', filename)
