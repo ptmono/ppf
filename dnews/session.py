2e03dee9bf40295fe46396fafb5a2ce0b417b99e
ptmono <ptmono@gmail.com>
2014-01-07 06:19:26 +0900

    Remove dnews

--- dnews/session.py
+++
@@ -1,9 +0,0 @@
-
-from sqlalchemy		import create_engine
-from sqlalchemy.orm	import sessionmaker, scoped_session
-
-from .mapper import metadata, MapperClass, ModelMapper
-
-class Session:
-    def __init__(self, smodel, engine_name):
-        pass
