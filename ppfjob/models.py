090776bd4563183632b7e64d67a879bdd85f0ebd
ptmono <ptmono@gmail.com>
2014-01-10 06:25:25 +0900

    nolog

--- ppfjob/models.py
+++ ppfjob/models.py
@@ -6,7 +6,7 @@ class Orms(object):
     __scraper = None
     def __new__(cls, *args, **kwargs):
         if not cls.__scraper:
-            scraper = Scraper(SaraminItModel, "sqlite:////home/ptmono/myscript/0services/dScraper/dScrapper/dbs/SaraminIt.sqlite")    
+            scraper = Scraper(SaraminItModel, "sqlite:////home/ptmono/myscript/0services/dScraper/dScraper/dbs/SaraminIt.sqlite")    
             orms = scraper.session.query(scraper.mapped_class).all()
             orms.reverse()
             cls.__scraper = orms
